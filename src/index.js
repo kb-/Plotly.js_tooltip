1459318d591076d31962b97bfa14b2333e901c59
kb- <olivier.around.the.world@gmail.com>
2024-04-21 19:50:08 +0200

    Added annotation deletion functionality

--- src/index.js
+++ src/index.js
@@ -26,10 +26,21 @@ var trace2 = {
 var data = [trace1, trace2];
 
 layout = {
-	//hovertemplate:template
+	annotations: [] // Ensure annotations array is initialized
 }
 
-Plotly.newPlot(plotId, data, layout);
+// Configuration for editable components
+var config = {
+  editable: true,
+  // edits: {
+    // annotationPosition: true,
+    // annotationTail: true,
+    // annotationText: true,
+    // axisTitleText: true
+  // }
+};
+
+Plotly.newPlot(plotId, data, layout, config);
 	
 document.getElementById(plotId).on('plotly_click', function(data){
     var pts = data.points[0];
@@ -50,4 +61,21 @@ document.getElementById(plotId).on('plotly_click', function(data){
     };
     existingAnnotations.push(newAnnotation);  // Add new annotation to the array
     Plotly.relayout(gd, { annotations: existingAnnotations });  // Update the plot with the new annotations array
+});
+
+// Tooltip deletion
+document.getElementById(plotId).on('plotly_relayout', function(eventData){
+    var gd = document.getElementById(plotId);
+    var updatedAnnotations = gd.layout.annotations.filter((anno, index) => {
+        const key = `annotations[${index}].text`;
+        return eventData[key] !== ''; // Keep annotation if its text is not empty
+    });
+
+    if (gd.layout.annotations.length !== updatedAnnotations.length) { // Check if any annotations were removed
+        Plotly.relayout(gd, {
+            annotations: updatedAnnotations
+        }).then(function() {
+            console.log('Empty annotations removed');
+        });
+    }
 });
