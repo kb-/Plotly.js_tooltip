5500cfb78fdc4e93254ae99e0b8746230543eae7
kb- <olivier.around.the.world@gmail.com>
2024-04-22 01:18:06 +0200

    Styling and defaults handling

--- src/index.js
+++ src/index.js
@@ -1,66 +1,99 @@
 // src/index.js
+const _ = require('lodash');
 const Plotly = require('plotly.js-basic-dist');
 console.log(Plotly); // Check if Plotly is defined
-const { lib } = require('plotly.js/src/lib/index');
+const {lib} = require('plotly.js/src/lib/index');
 window.lib = lib;
 console.log(lib);
 
 const plotId = 'plot'; // This ID must match the ID of the div containing the Plotly plot
 
-template = "y: %{y}"; //"%H~%M~%S.%2f"
+var userTemplate = "y: %{y}"; //"%H~%M~%S.%2f"
+var custom_style = {
+  arrowsize: 1.2,
+  font: {
+    size: 10,
+	color:'blue',
+  },
+};
+
+const DEFAULT_TEMPLATE = "x: %{x},<br>y: %{y}";
+const DEFAULT_STYLE = {
+  align: "left",// horizontal alignment of the text (can be 'left', 'center', or 'right')
+  arrowcolor: "black", // color of the annotation arrow
+  arrowhead: 3,// type of arrowhead, for Plotly (an integer from 0 to 8)
+  arrowsize: 1.8,// relative size of the arrowhead to the arrow stem, for Plotly
+  arrowwidth: 1,// width of the annotation arrow in pixels, for Plotly
+  font: {
+    color: "black",// color of the annotation text
+    family: "Arial",// font family of the annotation text, for Plotly
+    size: 12,// size of the annotation text in points, for Plotly
+  },
+  showarrow: true,
+  xanchor: "left",// horizontal alignment of the text (can be 'left', 'center', or 'right')
+};
+
+_.defaults(userTemplate, DEFAULT_TEMPLATE);
+_.defaults(custom_style, DEFAULT_STYLE);
 
 var trace1 = {
   x: [1, 2, 3, 4],
   y: [10, 15, 13, 17],
   type: 'scatter',
-  hovertemplate: template,
+  hovertemplate: userTemplate,
 };
 
 var trace2 = {
   x: [1, 2, 3, 4],
   y: [16, 5, 11, 9],
   type: 'scatter',
-  hovertemplate: template,
+  hovertemplate: userTemplate,
 };
 
 var data = [trace1, trace2];
 
 layout = {
-	annotations: [] // Ensure annotations array is initialized
+  annotations: [] // Ensure annotations array is initialized
 }
 
 // Configuration for editable components
 var config = {
   editable: true,
   // edits: {
-    // annotationPosition: true,
-    // annotationTail: true,
-    // annotationText: true,
-    // axisTitleText: true
+  // annotationPosition: true,
+  // annotationTail: true,
+  // annotationText: true,
+  // axisTitleText: true
   // }
 };
 
 Plotly.newPlot(plotId, data, layout, config);
-	
-document.getElementById(plotId).on('plotly_click', function(data){
-    var pts = data.points[0];
-    var gd = document.getElementById(plotId);
-    var existingAnnotations = (gd.layout.annotations || []).slice(); // Clone the array to avoid direct mutation
-	var text = lib.hovertemplateString(template, {}, gd._fullLayout._d3locale, pts, {});
 
-    var newAnnotation = {
-        x: pts.x,
-        y: pts.y,
-        xref: 'x',
-        yref: 'y',
-        text: text,
-        showarrow: true,
-        arrowhead: 7,
-        ax: 0,
-        ay: -40
-    };
-    existingAnnotations.push(newAnnotation);  // Add new annotation to the array
-    Plotly.relayout(gd, { annotations: existingAnnotations });  // Update the plot with the new annotations array
+document.getElementById(plotId).on('plotly_click',
+function(data) {
+  var pts = data.points[0];
+  var gd = document.getElementById(plotId);
+  var existingAnnotations = (gd.layout.annotations || []).slice(); // Clone the array to avoid direct mutation
+  var text = lib.hovertemplateString(userTemplate, {},
+  gd._fullLayout._d3locale, pts, {});
+
+  var newAnnotation = {
+    x: pts.x,
+    y: pts.y,
+    xref: 'x',
+    yref: 'y',
+    text: text,
+    showarrow: true,
+    ax: 5,
+    ay: -20
+  };
+  
+  _.defaults(newAnnotation, custom_style);
+
+  existingAnnotations.push(newAnnotation); // Add new annotation to the array
+  Plotly.relayout(gd, {
+    annotations: existingAnnotations
+  }); // Update the plot with the new annotations array
 });
 
 // Tooltip deletion
