62bfdc737a345c3d2809b014ca01954d87a1e698
kb- <olivier.around.the.world@gmail.com>
2024-05-16 01:21:53 +0200

    fix and compile

--- dist/plotly_tooltip.js
+++ dist/plotly_tooltip.js
@@ -38062,11 +38062,9 @@ function getTraceType(traceType) {
   \**************************/
 /***/ ((module, __unused_webpack_exports, __webpack_require__) => {
 
-// src/lib/index.js
-const _ = __webpack_require__(/*! lodash */ "./node_modules/lodash/lodash.js");
-const { lib } = __webpack_require__(/*! plotly.js/src/lib/index */ "./node_modules/plotly.js/src/lib/index.js");//edited Plotly source with added lib export
-
-const DEFAULT_TEMPLATE = "x: %{x},<br>y: %{y}";
+const { lib } = __webpack_require__(/*! plotly.js/src/lib/index */ "./node_modules/plotly.js/src/lib/index.js"); // Assume Plotly's internal API is used for hovertemplate processing
+const _ = __webpack_require__(/*! lodash */ "./node_modules/lodash/lodash.js"); // Utility library for extending default settings
+const DEFAULT_TEMPLATE = "x: %{x},<br>y: %{y}"; // Default tooltip template
 const DEFAULT_STYLE = {
   align: "left",
   arrowcolor: "black",
@@ -38082,117 +38080,75 @@ const DEFAULT_STYLE = {
   xanchor: "left",
 };
 
-// Custom SVG icon path for the tooltip toggle button
-const customIconPath = "M3.559 4.544c.355-.35.834-.544 1.33-.544H19.11c.496 0 .975.194 1.33.544s.559.829.559 1.331v9.25c0 .502-.203.981-.559 1.331-.355.35-.834.544-1.33.544H15.5l-2.7 3.6a1 1 0 0 1-1.6 0L8.5 17H4.889c-.496 0-.975-.194-1.33-.544A1.87 1.87 0 0 1 3 15.125v-9.25c0-.502.203-.981.559-1.331M7.556 7.5a1 1 0 1 0 0 2h8a1 1 0 0 0 0-2zm0 3.5a1 1 0 1 0 0 2H12a1 1 0 1 0 0-2z";
-
-const customIcon = {
-    width: 24,
-    height: 24,
-    path: customIconPath,
-};
-
-// State variable to keep track of tooltips activation status
-let tooltipsEnabled = false;
-
-/**
- * Creates a tooltip toggle button for Plotly charts.
- * @param {string} plotId - The ID of the DOM element containing the plot.
- * @param {string} userTemplate - Custom HTML template for tooltip content.
- * @param {Object} customStyle - Custom style settings for tooltip appearance.
- * @returns {Object} An object containing the button configuration.
- */
-function Plotly_Tooltip(plotId, userTemplate, customStyle) {
-  // Merge custom styles with default styles
-  _.defaults(customStyle, DEFAULT_STYLE);
-
-  return {
-    name: 'toggleTooltip',
-    title: 'Add tooltip',
-    icon: customIcon,
-    click: function(gd) {
-      toggleTooltipFunctionality(plotId, userTemplate, customStyle);
+// Function to apply tooltip functionality
+function applyTooltipFunctionality(gd, userTemplate, customStyle) {
+  const template = userTemplate || DEFAULT_TEMPLATE;
+  gd.on('plotly_click', function(data) {
+    var pts = data.points[0];
+    var existingIndex = gd.layout.annotations.findIndex(ann => ann.x === pts.x && ann.y === pts.y);
+    var text = lib.hovertemplateString(template, {}, gd._fullLayout._d3locale, pts, {});
+    var newAnnotation = {
+      x: pts.x,
+      y: pts.y,
+      xref: 'x',
+      yref: 'y',
+      text: text,
+      showarrow: true,
+      ax: 5,
+      ay: -20,
+    };
+    _.defaults(newAnnotation, customStyle);
+    if (existingIndex === -1) {
+      gd.layout.annotations.push(newAnnotation);
+      Plotly.relayout(gd, { annotations: gd.layout.annotations });
     }
-  };
+  });
+
+  // Set up to only run once
+  if (!gd._hasTooltipHandler) {
+    gd._hasTooltipHandler = true;
+
+    // Delete annotation when editable annotation text gets deleted
+    gd.on('plotly_relayout', function(eventData) {
+      // Iterate over eventData to find which annotation's text was set to empty
+      for (let key in eventData) {
+          if (key.includes('annotations[') && key.includes('].text')) {
+              const index = key.match(/annotations\[(\d+)\]\.text/)[1];
+              if (eventData[key] === '') { // Check if the text is set to empty
+                  var updatedAnnotations = gd.layout.annotations || [];
+                  updatedAnnotations.splice(index, 1); // Remove the annotation at the specific index
+                  Plotly.relayout(gd, { annotations: updatedAnnotations });
+                  break; // Exit after handling the specific empty text case
+              }
+          }
+      }
+    });
+  }
 }
 
-must_run = true; // Flag to ensure certain functionality runs only once
-
-/**
- * Toggles tooltip functionality on a Plotly plot based on user interaction.
- * @param {string} plotId - The ID of the DOM element containing the plot.
- * @param {string} userTemplate - Custom HTML template for tooltip content.
- * @param {Object} customStyle - Custom style settings for tooltip appearance.
- */
-const toggleTooltipFunctionality = (plotId, userTemplate, customStyle) => {
-    // Apply default styles if custom styles are not provided
-    _.defaults(customStyle, DEFAULT_STYLE);
-    userTemplate = userTemplate?userTemplate:DEFAULT_TEMPLATE;
-    const gd = document.getElementById(plotId);
-    
-    // Initialize the annotations array if it doesn't exist
-    if(gd.layout.annotations===undefined)gd.layout.annotations = [];
-    
-    // Logic to add or remove tooltips based on current state
-    if (!tooltipsEnabled) {
-        // Activate tooltips
-        gd.on('plotly_click', function(data) {
-            var pts = data.points[0];
-            var existingIndex = gd.layout.annotations.findIndex(ann => ann.x === pts.x && ann.y === pts.y);
-            
-            // Convert template to text using with Plotly hovertemplate formatting method
-            var text = lib.hovertemplateString(userTemplate, {}, gd._fullLayout._d3locale, pts, {});
-
-            var newAnnotation = {
-                x: pts.x,
-                y: pts.y,
-                xref: 'x',
-                yref: 'y',
-                text: text,
-                showarrow: true,
-                ax: 5,
-                ay: -20,
-            };
-            
-            // Apply style to annotation
-            _.defaults(newAnnotation, customStyle);
-            
-            // Add annotation to point if not annotated
-            if (existingIndex === -1) {
-                gd.layout.annotations.push(newAnnotation);
-                Plotly.relayout(gd, { annotations: gd.layout.annotations });
-            }
+// Initialize tooltips or set up observer if element is not yet available
+function initializeTooltips(plotId, userTemplate, customStyle) {
+  let gd = document.getElementById(plotId);
+  if (gd) {
+    applyTooltipFunctionality(gd, userTemplate, customStyle);
+  } else {
+    // Observer to handle dynamically added DOM elements
+    const observer = new MutationObserver(function(mutations) {
+      mutations.forEach(function(mutation) {
+        mutation.addedNodes.forEach(function(node) {
+          if (node.id === plotId && node.tagName === 'DIV') { // Ensure it matches the expected plot container
+            applyTooltipFunctionality(node, userTemplate, customStyle);
+            observer.disconnect(); // Disconnect after successful application to avoid unnecessary overhead
+          }
         });
-        tooltipsEnabled = true;
-    } else {
-        // Deactivate tooltips
-        gd.removeAllListeners('plotly_click');
-        tooltipsEnabled = false;
-    }
-    
-    if(must_run == true){//run once
-        must_run = false;
-        
-        // Delete annotation when editable annotation text gets deleted
-        gd.on('plotly_relayout', function(eventData) {
-            // Iterate over eventData to find which annotation's text was set to empty
-            for (let key in eventData) {
-                if (key.includes('annotations[') && key.includes('].text')) {
-                    const index = key.match(/annotations\[(\d+)\]\.text/)[1];
-                    if (eventData[key] === '') { // Check if the text is set to empty
-                        var gd = document.getElementById(plotId);
-                        var updatedAnnotations = gd.layout.annotations===undefined?[]:gd.layout.annotations;
-                        updatedAnnotations.splice(index, 1); // Remove the annotation at the specific index
-                        Plotly.relayout(gd, { annotations: updatedAnnotations });
-                        break; // Exit after handling the specific empty text case
-                    }
-                }
-            }
-        });
-    }
-};
+      });
+    });
+    observer.observe(document.body, { childList: true, subtree: true });
+  }
+}
 
 module.exports = {
-  Plotly_Tooltip
+  initializeTooltips
 };
 
 
--- src/lib/index.js
+++ src/lib/index.js
@@ -1,9 +1,6 @@
-
-// src/lib/index.js
-const _ = require('lodash');
-const { lib } = require('plotly.js/src/lib/index'); //edited Plotly source with added lib export
-
-const DEFAULT_TEMPLATE = "x: %{x},<br>y: %{y}";
+const { lib } = require('plotly.js/src/lib/index'); // Assume Plotly's internal API is used for hovertemplate processing
+const _ = require('lodash'); // Utility library for extending default settings
+const DEFAULT_TEMPLATE = "x: %{x},<br>y: %{y}"; // Default tooltip template
 const DEFAULT_STYLE = {
   align: "left",
   arrowcolor: "black",
@@ -19,24 +16,73 @@ const DEFAULT_STYLE = {
   xanchor: "left",
 };
 
-// State variable to keep track of tooltips activation status
-let tooltipsEnabled = true; // Tooltips are always enabled
+// Function to apply tooltip functionality
+function applyTooltipFunctionality(gd, userTemplate, customStyle) {
+  const template = userTemplate || DEFAULT_TEMPLATE;
+  gd.on('plotly_click', function(data) {
+    var pts = data.points[0];
+    var existingIndex = gd.layout.annotations.findIndex(ann => ann.x === pts.x && ann.y === pts.y);
+    var text = lib.hovertemplateString(template, {}, gd._fullLayout._d3locale, pts, {});
+    var newAnnotation = {
+      x: pts.x,
+      y: pts.y,
+      xref: 'x',
+      yref: 'y',
+      text: text,
+      showarrow: true,
+      ax: 5,
+      ay: -20,
+    };
+    _.defaults(newAnnotation, customStyle);
+    if (existingIndex === -1) {
+      gd.layout.annotations.push(newAnnotation);
+      Plotly.relayout(gd, { annotations: gd.layout.annotations });
+    }
+  });
 
-/**
- * Initialize tooltip functionality for Plotly charts.
- * @param {string} plotId - The ID of the DOM element containing the plot.
- * @param {string} userTemplate - Custom HTML template for tooltip content.
- * @param {Object} customStyle - Custom style settings for tooltip appearance.
- */
-function initializeTooltips(plotId, userTemplate, customStyle) {
-  // Merge custom styles with default styles
-  _.defaults(customStyle, DEFAULT_STYLE);
+  // Set up to only run once
+  if (!gd._hasTooltipHandler) {
+    gd._hasTooltipHandler = true;
 
-  // Automatically enable tooltip functionality
-  enableTooltipFunctionality(plotId, userTemplate, customStyle);
+    // Delete annotation when editable annotation text gets deleted
+    gd.on('plotly_relayout', function(eventData) {
+      // Iterate over eventData to find which annotation's text was set to empty
+      for (let key in eventData) {
+          if (key.includes('annotations[') && key.includes('].text')) {
+              const index = key.match(/annotations\[(\d+)\]\.text/)[1];
+              if (eventData[key] === '') { // Check if the text is set to empty
+                  var updatedAnnotations = gd.layout.annotations || [];
+                  updatedAnnotations.splice(index, 1); // Remove the annotation at the specific index
+                  Plotly.relayout(gd, { annotations: updatedAnnotations });
+                  break; // Exit after handling the specific empty text case
+              }
+          }
+      }
+    });
+  }
 }
 
-// Expose the initialize function
+// Initialize tooltips or set up observer if element is not yet available
+function initializeTooltips(plotId, userTemplate, customStyle) {
+  let gd = document.getElementById(plotId);
+  if (gd) {
+    applyTooltipFunctionality(gd, userTemplate, customStyle);
+  } else {
+    // Observer to handle dynamically added DOM elements
+    const observer = new MutationObserver(function(mutations) {
+      mutations.forEach(function(mutation) {
+        mutation.addedNodes.forEach(function(node) {
+          if (node.id === plotId && node.tagName === 'DIV') { // Ensure it matches the expected plot container
+            applyTooltipFunctionality(node, userTemplate, customStyle);
+            observer.disconnect(); // Disconnect after successful application to avoid unnecessary overhead
+          }
+        });
+      });
+    });
+    observer.observe(document.body, { childList: true, subtree: true });
+  }
+}
+
 module.exports = {
   initializeTooltips
 };
